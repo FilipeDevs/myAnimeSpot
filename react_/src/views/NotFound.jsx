89dbf3abfc4d4effc593c9636d81f7a531a6ada0
Filipe Pereira <filipe.pereira.dev@gmail.com>
2023-10-28 22:23:10 +0200

    Add some style to 404

--- react_/src/views/NotFound.jsx
+++ react_/src/views/NotFound.jsx
@@ -1,8 +1,16 @@
-
 function NotFound() {
-  return (
-    <div>404 - Page Not Found !</div>
-  )
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+            <p className="text-6xl text-red-600 font-bold">404</p>
+            <p className="text-4xl text-gray-800 mt-2">Page Not Found</p>
+            <p className="text-gray-600 mt-4 text-xl">
+                The page you are looking for does not exist :(
+            </p>
+            <a href="/" className="text-gray-600 mt-4 text-xl underline">
+                Go back to Home
+            </a>
+        </div>
+    );
 }
 
-export default NotFound
+export default NotFound;
