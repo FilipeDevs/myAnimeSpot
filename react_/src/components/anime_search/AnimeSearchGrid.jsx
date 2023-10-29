29becd9daf03d76b7e5477a7fa6ecbb240676247
Filipe Pereira <filipe.pereira.dev@gmail.com>
2023-10-29 18:03:30 +0100

    Add load extra content feature to search view

--- react_/src/components/anime_search/AnimeSearchGrid.jsx
+++ react_/src/components/anime_search/AnimeSearchGrid.jsx
@@ -1,4 +1,5 @@
-import { useQuery } from "@apollo/client";
+import React, { useEffect } from "react";
+import { useInfiniteQuery, useQueryClient } from "react-query";
 import apolloClient from "../../apollo-client";
 import searchAnimeQuery from "../../queries/searchqlQueries";
 import AnimeSearchCard from "./AnimeSearchCard";
@@ -9,7 +10,6 @@ import Error from "../../views/Error";
 
 function AnimeSearchGrid() {
     const location = useLocation();
-
     const queryParams = {};
     const urlSearchParams = new URLSearchParams(location.search);
 
@@ -17,42 +17,88 @@ function AnimeSearchGrid() {
         queryParams[key] = value;
     }
 
-    const { data, loading, error } = useQuery(searchAnimeQuery, {
-        client: apolloClient,
-        variables: {
-            page: 1,
-            perPage: 30,
-            ...queryParams,
+    const queryClient = useQueryClient();
+
+    useEffect(() => {
+        return () => {
+            /**
+             * Clear the query cache when the component unmounts.
+             * This is important to ensure that React Query doesn't store and
+             * reuse previously loaded content when the user navigates away from
+             * this page and returns. Without clearing the cache, the additional
+             * loaded content would be retained, resulting in unnecessary API
+             * requests and potential data redundancy.
+             */
+            queryClient.removeQueries("searchAnime");
+        };
+    }, []);
+
+    const {
+        data,
+        fetchNextPage,
+        hasNextPage,
+        isFetchingNextPage,
+        isLoading,
+        error,
+    } = useInfiniteQuery(
+        "searchAnime",
+        async ({ pageParam = 1 }) => {
+            const response = await apolloClient.query({
+                query: searchAnimeQuery,
+                variables: {
+                    page: pageParam,
+                    perPage: 15,
+                    ...queryParams,
+                },
+            });
+            return response.data.Page.media;
         },
-    });
+        {
+            getNextPageParam: (lastPage, allPages) => {
+                const nextPage =
+                    lastPage.length > 0 ? allPages.length + 1 : null;
+                return nextPage;
+            },
+        }
+    );
 
-    if (loading) return <Loading />;
-
+    if (isLoading) return <Loading />;
     if (error) return <Error />;
 
     return (
         <div className="mb-10">
             <div className="flex flex-col items-center justify-center py-6">
-                {data.Page.media.length == 0 && <NothingFound />}
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 xl:grid-cols-3 gap-4 md:grid-cols-1 gap-4 sm:grid-cols-1 gap-3">
-                    {data.Page.media.map((anime) => {
-                        return (
-                            <AnimeSearchCard
-                                key={anime.id}
-                                id={anime.id}
-                                image={anime.coverImage.large}
-                                title={[
-                                    anime.title.english,
-                                    anime.title.romaji,
-                                ]}
-                                description={anime.description}
-                                genres={anime.genres}
-                                seasonYear={anime.seasonYear}
-                                season={anime.season}
-                            />
-                        );
-                    })}
+                {data.pages[0]?.length === 0 && <NothingFound />}
+                <div className="grid grid-cols-1 gap-3 lg:grid-cols-2 xl:grid-cols-3 md:grid-cols-1 sm:grid-cols-1">
+                    {data.pages.map((page, pageIndex) => (
+                        <React.Fragment key={pageIndex}>
+                            {page.map((anime) => (
+                                <AnimeSearchCard
+                                    key={anime.id}
+                                    id={anime.id}
+                                    image={anime.coverImage.large}
+                                    title={[
+                                        anime.title.english,
+                                        anime.title.romaji,
+                                    ]}
+                                    description={anime.description}
+                                    genres={anime.genres}
+                                    seasonYear={anime.seasonYear}
+                                    season={anime.season}
+                                />
+                            ))}
+                        </React.Fragment>
+                    ))}
                 </div>
+                {hasNextPage && (
+                    <button
+                        className="px-3 py-2 mt-2 text-sm font-medium text-white bg-blue-700 rounded hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        onClick={() => fetchNextPage()}
+                        disabled={isFetchingNextPage}
+                    >
+                        {isFetchingNextPage ? "Loading..." : "Load More"}
+                    </button>
+                )}
             </div>
         </div>
     );
